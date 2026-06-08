8f8121b1c656e632fab097eb6c92d24c5e23b6c8
Rodrigo <digojb22@gmail.com>
2026-06-08 19:49:17 -0300

    Codigo mais organizado

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,34 +1,34 @@
 import java.util.Scanner;
- public class HelloWorld{
+  public class HelloWorld{
      public static void main (String[] args){
-         Scanner scanner = new Scanner(System.in);
+      Scanner scanner = new Scanner(System.in);
          
-            System.out.println ("---------------------------------");
-            System.out.println ("             Médias          ");
+         System.out.println ("---------------------------------");
+          System.out.println ("             Médias          ");
             System.out.println ("---------------------------------"); 
-            System.out.println ("Informe seu nome: ");
+             System.out.println ("Informe seu nome: ");
             
-              String nome = scanner.nextLine();
+               String nome = scanner.nextLine();
               
-               System.out.println (nome + ", informe sua primeira nota:");
-                double n1 = scanner.nextDouble();
+                System.out.println (nome + ", informe sua primeira nota:");
+                 double n1 = scanner.nextDouble();
               
-               System.out.println ("Infome sua segunda nota:");
-                double n2 = scanner.nextDouble();
+                  System.out.println ("Infome sua segunda nota:");
+                   double n2 = scanner.nextDouble();
               
-               System.out.println ("Por fim, informe sua última nota:");
-                double n3 = scanner.nextDouble();
+                    System.out.println ("Por fim, informe sua última nota:");
+                     double n3 = scanner.nextDouble();
               
-                 double M = (n1 + n2 + n3) / 3; 
+                      double M = (n1 + n2 + n3) / 3; 
               
-                  if (M >= 7) 
-                   {System.out.printf (nome + ", Parabéns, você foi aprovado com média:  %.1f",  M);}
-                    else {System.out.printf (nome + ", você não atingiu a média... Sua média foi: %.1f", M);}
+                       if (M >= 7) 
+                        {System.out.printf (nome + ", Parabéns, você foi aprovado com média:  %.1f",  M);}
+                         else {System.out.printf (nome + ", você não atingiu a média... Sua média foi: %.1f", M);}
               
           /*Lembrar de fechar o scanner!
           Lembrar de usar o %2.f para casa decimais,
           com o printf!*/ 
           
-           scanner.close();   
+                          scanner.close();   
      }
  }
